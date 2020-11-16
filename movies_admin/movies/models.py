39c3c0d3b4a95a414557d5f190b0b9070244919c
mrvanolog <mrvanolog97@gmail.com>
2020-11-17 01:06:44 +0300

    changed model to the one that is consistent with schema_design

--- movies_admin/movies/models.py
+++ movies_admin/movies/models.py
@@ -1,96 +1,95 @@
+import uuid
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django.core.validators import MinValueValidator
 from model_utils.models import TimeStampedModel
 
 
-class Director(TimeStampedModel):
-    first_name = models.CharField(_("имя"), max_length=255)
-    last_name = models.CharField(_("фамилия"), max_length=255)
+CONTENT_SCHEMA = "content"
+
+
+class Person(TimeStampedModel):
+    id = models.UUIDField(_("id"), primary_key=True, default=uuid.uuid4, editable=False)
+    full_name = models.TextField(_("полное имя"))
+    birth_date = models.DateField(_("дата рождения"), blank=True)
 
     class Meta:
-        verbose_name = _("режиссер")
-        verbose_name_plural = _("режиссеры")
+        verbose_name = _("персона")
+        verbose_name_plural = _("персоны")
+        db_table = f"{CONTENT_SCHEMA}.person"
 
     def __str__(self):
-        return self.first_name + " " + self.last_name
-
-
-class Actor(TimeStampedModel):
-    first_name = models.CharField(_("имя"), max_length=255)
-    last_name = models.CharField(_("фамилия"), max_length=255)
-
-    class Meta:
-        verbose_name = _("актер")
-        verbose_name_plural = _("актеры")
-
-    def __str__(self):
-        return self.first_name + " " + self.last_name
-
-
-class Writer(TimeStampedModel):
-    first_name = models.CharField(_("имя"), max_length=255)
-    last_name = models.CharField(_("фамилия"), max_length=255)
-
-    class Meta:
-        verbose_name = _("сценарист")
-        verbose_name_plural = _("сценаристы")
-
-    def __str__(self):
-        return self.first_name + " " + self.last_name
+        return self.full_name
 
 
 class Genre(TimeStampedModel):
-    name = models.CharField(_("название"), max_length=255)
+    id = models.UUIDField(_("id"), primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.TextField(_("название"))
     description = models.TextField(_("описание"), blank=True)
 
     class Meta:
         verbose_name = _("жанр")
         verbose_name_plural = _("жанры")
+        db_table = f"{CONTENT_SCHEMA}.genre"
 
     def __str__(self):
         return self.name
 
 
-class Movie(TimeStampedModel):
+class FilmWorkType(models.TextChoices):
+    MOVIE = "film", _("фильм")
+    SERIES = "series", _("сериал")
+
+
+class FilmWork(TimeStampedModel):
+    id = models.UUIDField(_("id"), primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(_("название"), max_length=255)
     description = models.TextField(_("описание"), blank=True)
     creation_date = models.DateField(_("дата создания фильма"), blank=True)
-    age_restriction = models.IntegerField(
-        _("возрастной ценз"), validators=[MinValueValidator(0)], blank=True
-    )
-    rating = models.FloatField(_("рейтинг"), validators=[MinValueValidator(0)], blank=True)
-    directors = models.ManyToManyField(Director)
-    actors = models.ManyToManyField(Actor)
-    writers = models.ManyToManyField(Writer)
-    genres = models.ManyToManyField(Genre)
+    certificate = models.TextField(_("сертификат"), blank=True)
     file_path = models.FileField(_("файл"), upload_to="film_works/", blank=True)
+    rating = models.FloatField(_("рейтинг"), validators=[MinValueValidator(0)], blank=True)
+    type = models.TextField(_("тип"), choices=FilmWorkType.choices, blank=True)
+    genres = models.ManyToManyField("movies.Genre", through="movies.GenreFilmWork")
+    persons = models.ManyToManyField("movies.Person", through="movies.PersonFilmWork")
 
     class Meta:
-        verbose_name = _("фильм")
-        verbose_name_plural = _("фильмы")
+        verbose_name = _("кинопроизведение")
+        verbose_name_plural = _("кинопроизведения")
+        db_table = f"{CONTENT_SCHEMA}.film_work"
 
     def __str__(self):
         return self.title
 
 
-class Series(TimeStampedModel):
-    title = models.CharField(_("название"), max_length=255)
-    description = models.TextField(_("описание"), blank=True)
-    creation_date = models.DateField(_("дата создания фильма"), blank=True)
-    age_restriction = models.IntegerField(
-        _("возрастной ценз"), validators=[MinValueValidator(0)], blank=True
-    )
-    rating = models.FloatField(_("рейтинг"), validators=[MinValueValidator(0)], blank=True)
-    directors = models.ManyToManyField(Director)
-    actors = models.ManyToManyField(Actor)
-    writers = models.ManyToManyField(Writer)
-    genres = models.ManyToManyField(Genre)
-    file_path = models.FileField(_("файл"), upload_to="film_works/", blank=True)
+class RoleType(models.TextChoices):
+    ACTOR = "actor", _("актер")
+    WRITER = "writer", _("сценарист")
+    DIRECTOR = "director", _("режиссер")
+
+
+class PersonFilmWork(models.Model):
+    id = models.UUIDField(_("id"), primary_key=True, default=uuid.uuid4, editable=False)
+    film_work = models.ForeignKey("movies.FilmWork", on_delete=models.CASCADE)
+    person = models.ForeignKey("movies.Person", on_delete=models.CASCADE)
+    role = models.TextField(_("роль"), choices=RoleType.choices)
+    created = models.DateTimeField(_("дата создания"), auto_created=True, auto_now_add=True)
 
     class Meta:
-        verbose_name = _("сериал")
-        verbose_name_plural = _("сериалы")
+        verbose_name = _("участник кинопроизведение")
+        verbose_name_plural = _("участники кинопроизведения")
+        db_table = f"{CONTENT_SCHEMA}.person_film_work"
+        unique_together = ("film_work", "person", "role")
 
-    def __str__(self):
-        return self.title
+
+class GenreFilmWork(models.Model):
+    id = models.UUIDField(_("id"), primary_key=True, default=uuid.uuid4, editable=False)
+    film_work = models.ForeignKey("movies.FilmWork", on_delete=models.CASCADE)
+    genre = models.ForeignKey("movies.Genre", on_delete=models.CASCADE)
+    created = models.DateTimeField(_("дата создания"), auto_created=True, auto_now_add=True)
+
+    class Meta:
+        verbose_name = _("жанр кинопроизведение")
+        verbose_name_plural = _("жанры кинопроизведения")
+        db_table = f"{CONTENT_SCHEMA}.genre_film_work"
+        unique_together = ("film_work", "genre")
